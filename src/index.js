941398dd9c0e4b33704d1293c37cecd4b34116fb
Korir Carlos <carlkorir226@gmail.com>
2025-03-22 00:09:41 +0300

    adds DOMContentLoaded and fetch char names from APIs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // Your code here
+document.addEventListener('DOMContentLoaded',main)
+
+const topDiv = document.getElementById("character-bar")
+
+function fetchNames (){
+    return fetch("http://localhost:3000/characters")
+    .then((res)=> res.json())
+    .then(theNames)
+}
+function theNames(characters){
+    characters.forEach(character => {
+        const nameSpan = document.createElement("span")
+        nameSpan.innerHTML = character.name
+        topDiv.appendChild(nameSpan)
+    });
+}
+function main (){
+    fetchNames()
+}
